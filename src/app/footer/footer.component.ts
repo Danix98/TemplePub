ea7ffaea9b0050f7b696e15d51e79ffc8cbe6fe5
daniele <danielegiangiuli@gmail.com>
2022-12-10 18:30:40 +0100

    Initial Commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-//import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-//  faCoffee = faCoffee;
+
 }
