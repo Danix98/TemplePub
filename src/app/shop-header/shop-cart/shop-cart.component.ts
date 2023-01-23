95d9ce84c386f946adbb4c0f5cb92a3b2c3b2cb9
daniele <danielegiangiuli@gmail.com>
2023-01-23 22:19:31 +0100

    ShopCommit

--- src/app/shop-header/shop-cart/shop-cart.component.ts
+++ src/app/shop-header/shop-cart/shop-cart.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { DrinkService } from '../Drink.service';
 
@@ -14,8 +14,12 @@ export class ShopCartComponent implements OnInit {
 
   id: number;
   drink: DrinkService;
+  lPrice: number;
+  nPrice: number;
+  limitQnt = 50;
+  ngRemove: boolean;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private router_btn: Router, private route: ActivatedRoute) { }
 
 
   ngOnInit() {
@@ -27,5 +31,30 @@ export class ShopCartComponent implements OnInit {
         // this.shs.getDrinks();
       }
     )  
+  }
+
+  onCheckQnt(event: Event) {
+    this.lPrice = +(<HTMLInputElement>event.target).value
+    this. nPrice = this.lPrice *20000000;
+
+    if(this.lPrice > this.limitQnt || this.lPrice <1) {
+      return (this.nPrice = undefined)
+    }
+  }
+
+  onBuy() {
+    if(this.lPrice === undefined)
+      alert("Impossibile effettuare l'acquisto");
+    else {
+      alert("Hai acquistato: Dettagli");
+      this.router_btn.navigate(['shop']);
+    }
+  }
+
+  onCancel() {
+    this.ngRemove = confirm('Sei sicuro di rimuovere il carrello? I dati andranno persi.')
+
+    if(this.ngRemove === true)
+      this.router_btn.navigate(['shop']);
   }
 }
