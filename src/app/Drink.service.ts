1adbab86775aa2870ac7cf8c74170782e5ae2ad1
daniele <danielegiangiuli@gmail.com>
2023-02-08 18:20:32 +0100

    MainTweaks

--- src/app/Drink.service.ts
+++ src/app/Drink.service.ts
@@ -6,9 +6,9 @@ export class DrinkService {
     new Drink("prima birra", 'primo lorem', '../../assets/img/birra1.jpg', 10),
     new Drink("seconda birra", 'secondo lorem', '../../assets/img/birra2.jpg', 19),
     new Drink("terza birra", 'terzo lorem', '../../assets/img/birra3.jpg', 72),
-    new Drink(4, 'quarto lorem', '../../assets/img/l1.jpg', 99),
-    new Drink(5, 'quinto lorem', '../../assets/img/l2.jpg', 120),
-    new Drink(6, 'sesto lorem', '../../assets/img/l3.jpg', 150)
+    new Drink("quarto drink", 'quarto lorem', '../../assets/img/l1.jpg', 99),
+    new Drink("quinto drink", 'quinto lorem', '../../assets/img/l2.jpg', 120),
+    new Drink("sesto drink", 'sesto lorem', '../../assets/img/l3.jpg', 150)
   ];
 
   status: string|number;
