a32aae81869196002fd88b37fbba7619d479a12a
daniele <danielegiangiuli@gmail.com>
2023-05-16 18:04:21 +0200

    Wishlist

--- src/app/material/wishlist/wishlist.component.ts
+++ src/app/material/wishlist/wishlist.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrls: ['../../shared/generics.css']
+  styleUrls: ['./wishlist.component.css']
 })
 
 
